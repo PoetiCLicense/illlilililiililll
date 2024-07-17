2a7dd9f5ba3ddf1538ebb892275094d984c3b782
illlilililiililll <kcwrenew22c@gmail.com>
2024-07-17 09:45:54 +0900

    Edit Binary Search

--- 03. Search Algorithm/Binary Search/Binary Search (Recursion).c
+++ 03. Search Algorithm/Binary Search/Binary Search (Recursion).c
@@ -1,6 +1,26 @@
-#include <iostream>
+#include <stdio.h>
+
+int binary_search(int array[], int left, int right, int key) {
+    if (left <= right) {
+        int center = (left + right) / 2;
+
+        if (array[center] == key)
+            return center;
+
+        else if (array[center] > key)
+            return binary_search(array, left, center-1, key);
+
+        else
+            return binary_search(array, center+1, right, key);
+    }
+
+    return -1;
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int a[] = {1, 2, 3, 4, 5, 6, 7};
+    printf("%d\n", binary_search(a, 0, 6, 5));
+    printf("%d\n", binary_search(a, 0, 6, 8));
+
     return 0;
 }
